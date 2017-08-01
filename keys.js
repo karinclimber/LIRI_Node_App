4af8aaf45d95c29836b4d8d01347922420b3eb2c
Karin Hermann <karinclimber@gmail.com>
2017-08-01 17:27:52 -0400

    initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'vlW8zXWp4w5NL2bdHvB4BOxog',
+  consumer_secret: 'eJ6d4ev91FS76jVt3SP98dJPSg8XfqGLqsIUujtsg8BD4T2Twc',
+  access_token_key: '892146032305463297-VF8TsQ2OkoyrFNseRjJbZSBOblMvR8i',
+  access_token_secret: 'M08iWLshcbGLAAZWjQ4GTGG0Jlub3wfN3UhvXtG1YltkA',
+}
