0071f8a1efbb212e49f7e28e5e86c5a1f0f644a2
Karin Hermann <karinclimber@gmail.com>
2017-08-04 00:13:55 -0400

    partially working

--- keys.js
+++ keys.js
@@ -5,4 +5,13 @@ exports.twitterKeys = {
   consumer_secret: 'eJ6d4ev91FS76jVt3SP98dJPSg8XfqGLqsIUujtsg8BD4T2Twc',
   access_token_key: '892146032305463297-VF8TsQ2OkoyrFNseRjJbZSBOblMvR8i',
   access_token_secret: 'M08iWLshcbGLAAZWjQ4GTGG0Jlub3wfN3UhvXtG1YltkA',
+};
+
+exports.spotifyKeys = {
+	client_id: "ac809c8e803e4dd7a429690d5ff820ee",
+	client_secret: "9144e462618941209c478678951f5e85",
+};
+
+exports.omdbKey = {
+    key: "40e9cece"
 }
